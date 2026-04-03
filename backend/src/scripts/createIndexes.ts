2836d8587749488ab8f47c4f9a81c1c95ec2c688
akshayamin62 <akshay.amin.62@gmail.com>
2026-04-03 13:18:10 +0530

    chore: update typescript version to 6.0.2 in package.json and package-lock.json

--- backend/src/scripts/createIndexes.ts
+++ backend/src/scripts/createIndexes.ts
@@ -6,9 +6,6 @@ import '../models/Program';
 import '../models/Student';
 import '../models/User';
 import '../models/StudentServiceRegistration';
-import '../models/FormField';
-import '../models/FormSection';
-import '../models/FormSubSection';
 
 dotenv.config();
 
